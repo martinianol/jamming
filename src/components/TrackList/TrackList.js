5deefeb117ab77c1e525d67a2a60d48e972f8431
Mars <martinianol@outlook.com>
2021-09-29 19:39:32 -0300

    Track CSS - Step 28

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -19,5 +19,3 @@ class TrackList extends React.Component {
 };
 
 export default TrackList;
-
-/* Create a method called renderAction that displays a <button> element with - as its content if the isRemoval property is true, and a + <button> element if the isRemoval property is false. Set the class name to Track-action. */
