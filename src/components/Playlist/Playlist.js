587edcdce5f0b09097336382e74b0292cb6528db
Mars <martinianol@outlook.com>
2021-09-29 12:08:01 -0300

    SearchBar added

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Playlist.css';
+
+class PlayList extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  };
+};
+
+export default PlayList;
